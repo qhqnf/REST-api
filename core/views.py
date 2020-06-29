85902ce32e0259578e6231f0a2a29a8d1d69058f
root <didguscjf95@naver.com>
2020-06-29 18:33:39 +0900

    initialize django rest-api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
+from rooms.models import Room
 
-# Create your views here.
+
+def list_rooms(request):
+    data = serializers.serialize("json", Room.objects.all())
+    response = HttpResponse(content=data)
+    return response
