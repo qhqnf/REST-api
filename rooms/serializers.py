8f357db5be92178af6f431892dfe12a80fe8ced0
root <didguscjf95@naver.com>
2020-06-30 20:57:20 +0900

    create serializers

---
+++ rooms/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from users.serializers import TinyUserSerializer
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    user = TinyUserSerializer()
+
+    class Meta:
+        model = Room
+        fields = ("name", "price", "instant_book", "user")
