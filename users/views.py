bb4b64e57cab6e3eec538501f4369b3eb044ebd9
root <didguscjf95@naver.com>
2020-07-06 20:14:04 +0900

    create authentication with json web token

--- users/views.py
+++ users/views.py
@@ -80,9 +80,9 @@ def login(request):
         return Response(status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if user is not None:
-        encoding_jwt = jwt.encode(
-            {"id": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+        encoded_jwt = jwt.encode(
+            {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
         )
-        return Response(data=encoding_jwt)
+        return Response(data={"token": encoded_jwt})
     else:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
